bd74ed4e8f68d8929d827bdf161d01d1b09bb1bf
Florian Lenz <33814851+FlonairLenz@users.noreply.github.com>
2025-01-28 11:15:13 +0100

    fix function

--- src/DigitalReceiptBackend/GetReceipt/GetReceipt.cs
+++ src/DigitalReceiptBackend/GetReceipt/GetReceipt.cs
@@ -10,33 +10,14 @@ public class GetReceipt
     [Function("GetReceipt")]
     public IActionResult Run(
         [HttpTrigger(AuthorizationLevel.Function, nameof(HttpMethod.Get), Route = "receipts/{ReceiptId}")]
-        HttpRequest req)
-        //[CosmosDBInput("receipts", "receipts", Connection = "CosmosDBConnection", Id = "{ReceiptId}", PartitionKey = "{ReceiptId}", PreferredLocations = "%Location%")]
-        //Receipt? receipt)
+        HttpRequest req,
+        [CosmosDBInput("receipts", "receipts", Connection = "CosmosDBConnection", Id = "{ReceiptId}", PartitionKey = "{ReceiptId}", PreferredLocations = "%Location%")]
+        Receipt? receipt)
     {
-        var receipt = new Receipt
+        if (receipt == null)
         {
-            ReceiptNumber = req.RouteValues["ReceiptId"].ToString(),
-            ShopName = "Shop",
-            ReceiptDate = DateTime.UtcNow,
-            Items = new List<Item>
-            {
-                new Item
-                {
-                    Price = 1.23m,
-                    Quantity = 2,
-                    ProductName = "Product 1",
-                    Total = 2.46m
-                },
-                new Item
-                {
-                    Price = 2.23m,
-                    Quantity = 1,
-                    ProductName = "Product 2",
-                    Total = 2.23m
-                }
-            }
-        };
+            return new NotFoundResult();
+        }
         
         return new OkObjectResult(receipt);
     }
